cde8ce8528456709989f593db929c522f4d6481f
ZairaDamas <106176088+ZairaDamas@users.noreply.github.com>
2022-05-24 13:23:20 -0500

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Registros2] (
+
+    [Nombre]  NVARCHAR (20) NOT NULL,
+    [Apellido] NVARCHAR (20) NOT NULL,
+    [Fecha_Nacimiento]  DATE    NOT NULL,
+    [Correo]  NVARCHAR(50)          NULL,
+    [Usuario]  NVARCHAR (50) NULL,
+    [Contraseña]  NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Nombre] ASC)
+);
+
